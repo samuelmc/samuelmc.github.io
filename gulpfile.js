ffb8d34263edb2e120ff71e01ff4d6fb1961ad5c
Samuel Moncarey <samuel.moncarey@akkoo.com>
2017-03-07 13:33:02 +0100

    Add css

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+    'bower_components/normalize.scss/sass',
+    'bower_components/foundation-sites/scss',
+    'bower_components/foundation-perfect-scrollbar/src/scss/plugin',
+    'bower_components/foundation-select/src/scss/plugin',
+    'bower_components/motion-ui/src',
+    'bower_components/font-awesome/scss'
 ];
 
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+gulp.task('sass', function () {
+    return gulp.src('scss/app.scss')
+        .pipe($.sass({
+            includePaths: sassPaths,
+            outputStyle: 'compressed' // if css compressed **file size**
+        })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('default', ['sass'], function () {
+    gulp.watch(['scss/**/*.scss'], ['sass']);
 });
